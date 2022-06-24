ae5596b9cc7ad64a35e022a8d81784d7acdc1141
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-06-24 14:25:13 -0700

    lazy imports

---
+++ src/components/demos/Gltf.client.jsx
@@ -0,0 +1,27 @@
+import React, {Suspense, useEffect, useReducer} from 'react';
+import {Canvas, useLoader} from '@react-three/fiber';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+function Test() {
+  const [flag, toggle] = useReducer((state) => !state, true);
+  useEffect(() => {
+    const interval = setInterval(toggle, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  const {scene} = useLoader(
+    GLTFLoader,
+    flag ? '../../assets/Stork.glb' : '../../assets/Parrot.glb'
+  );
+  return <primitive object={scene} />;
+}
+
+export default function Gltf() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <Suspense fallback={null}>
+        <Test />
+      </Suspense>
+    </Canvas>
+  );
+}
---
+++ src/components/demos/Test.client.jsx
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import React, {useState, useEffect, useReducer} from 'react';
+import {Canvas, useFrame} from '@react-three/fiber';
+
+function TestModel() {
+  const [o1] = useState(
+    () =>
+      new THREE.Mesh(
+        new THREE.BoxGeometry(),
+        new THREE.MeshBasicMaterial({color: 'hotpink'})
+      )
+  );
+  const [o2] = useState(
+    () =>
+      new THREE.Mesh(
+        new THREE.BoxGeometry(),
+        new THREE.MeshBasicMaterial({color: 'aquamarine'})
+      )
+  );
+  const [which, toggle] = useReducer((state) => !state, true);
+  useEffect(() => {
+    const interval = setInterval(toggle, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useFrame((state) => {
+    console.log(state.pointer.x);
+  });
+
+  return <primitive object={which ? o1 : o2} />;
+}
+
+export default function Test() {
+  return (
+    <Canvas>
+      <TestModel />
+    </Canvas>
+  );
+}
