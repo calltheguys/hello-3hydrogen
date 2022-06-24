b0c07ecbcece925a7bc5f634dc85c9841b6d9862
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-06-24 12:12:02 -0700

    feat: add @netlify/hydrogen-platform

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
+import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
 
 export default defineConfig({
-  plugins: [hydrogen()],
+  plugins: [hydrogen(), netlifyPlugin()],
   optimizeDeps: {
     include: [
       'react-three-fiber',
