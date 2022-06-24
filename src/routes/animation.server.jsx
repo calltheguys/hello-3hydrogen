a4f571b141ba22ff8b1f04bd28e2986eacc37771
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-06-24 14:36:05 -0700

    fix: animation

--- src/routes/animation.server.jsx
+++ src/routes/animation.server.jsx
@@ -2,16 +2,16 @@ import {Link} from '@shopify/hydrogen';
 import {lazy, Suspense} from 'react';
 import {Loading} from '../components/index.server';
 
-const Animation = import.meta.env.SSR
-  ? Loading
-  : lazy(() => import('../components/demos/Animation.client.jsx'));
+const Animation = lazy(() =>
+  import('../components/demos/Animation.client.jsx')
+);
 
 export default function AnimationRoute() {
   return (
     <main>
       <h1>Animation</h1>
       <Link to="/">Back</Link>
-      <Suspense fallback={<Animation />}>
+      <Suspense fallback={<Loading />}>
         <Animation />
       </Suspense>
     </main>
